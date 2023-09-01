c8c3b1986d1b76319f41176c8cd34527d2d51b6b
bhawley45 <136668039+bhawley45@users.noreply.github.com>
2023-08-31 22:01:39 -0500

    Added new project folder, cleaned up ToDoList
    Added new "Calculator" project for upcoming assignment. Cleaned up "ToDoList" Project.

---
+++ Basic_C#_Projects/ToDoList/ToDoList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+
+Console.WriteLine("Provide a number: ");
+string userInput = Console.ReadLine();
+int num = int.Parse(userInput);
+Console.WriteLine(num);
+
+//Console.WriteLine("Hello!");
+//Console.WriteLine("What would you like to do?");
+//Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+//Console.WriteLine("[S]ee all TODOs");
+//Console.WriteLine("[A]dd a TODO");
+//Console.WriteLine("[R]emove a TODO");
+//Console.WriteLine("[E]xit");
+
+////reads line from console and assigns to variable
+//string userInput = Console.ReadLine();
+
+
+////require keypress to continue program, in this case terminate
+//Console.ReadKey();
