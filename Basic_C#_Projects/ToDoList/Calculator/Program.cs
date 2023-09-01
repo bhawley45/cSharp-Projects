359ae611fa55d7392397cd05b85ca8f253464de0
bhawley45 <136668039+bhawley45@users.noreply.github.com>
2023-09-01 18:14:36 -0500

    Finished basic calculator project
    Built, refactored, and commented code. Tested manually, no bugs to report. No invalid input handling on initial commands; user must input a valid number. Also, limited input validation on operation selection. Works with valid Strings and characters (A-z), upper and lower case, also has empty input handling.

--- Basic_C#_Projects/ToDoList/Calculator/Program.cs
+++ Basic_C#_Projects/ToDoList/Calculator/Program.cs
@@ -1,2 +1,95 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+//Print Welcome message
+Console.WriteLine("Hello! Welcome to the SimpleCalc 3000");
+
+//Ask User for FIRST Operand (Couldn't figure out how to overload GetOperand())
+int firstOperand = GetOperand(string.Empty);
+
+//Ask user for SECOND Operand
+int secondOperand = GetOperand(" second");
+
+//Ask user for operation, perform it, then print to console
+ValidateAndCalculate(firstOperand, secondOperand);
+
+//require keypress to continue program, in this case terminate
+Console.WriteLine("Press any key to close...");
+Console.ReadKey();
+
+
+int GetOperand(string notFirst)
+{
+    Console.WriteLine($"Input a{notFirst} number: ");
+    var userInput = Console.ReadLine();
+    return ConvertToInt(userInput);
+}
+
+//Convert to integer (NO invalid input handling)
+int ConvertToInt(string input)
+{
+    return int.Parse(input);
+ 
+}
+
+//Print options for avalable operations
+void PrintOptions()
+{
+    Console.WriteLine("What would you like to do with these numbers?");
+    Console.WriteLine("[A]dd");
+    Console.WriteLine("[S]ubtract");
+    Console.WriteLine("[M]ultiply");
+}
+
+
+void ValidateAndCalculate(int num1, int num2)
+{
+    string statement;
+    int result;
+
+    while (true)
+    {
+        //Print initial options and get user input
+        PrintOptions();
+        var userInput = Console.ReadLine();
+
+        //Handle null case first
+        if(string.IsNullOrEmpty(userInput))
+        {
+            Console.WriteLine("Please enter a value...");
+            continue;
+        }
+
+        //convert input to uppercase
+        userInput = userInput.ToUpper();
+
+        if(userInput == "A")
+        {
+            //Add Operands
+            result = num1+num2;
+            statement = $"{num1} + {num2} = ";
+            break;
+        }
+        else if(userInput == "S")
+        {
+            //Subtract Operands
+            result = num1 - num2;
+            statement = $"{num1} - {num2} = ";
+            break;
+        }
+        else if(userInput == "M")
+        {
+            //Multiply Operands
+            result = num1 * num2;
+            statement = $"{num1} x {num2} = ";
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Invalid choice...");
+            continue;
+        }
+    }
+
+    //Output results to console
+    Console.WriteLine(statement + result);
+}
--- Basic_C#_Projects/ToDoList/ToDoList/Program.cs
+++ Basic_C#_Projects/ToDoList/ToDoList/Program.cs
@@ -1,22 +1,16 @@
 ﻿using System.Diagnostics;
 
+Console.WriteLine("Hello!");
+Console.WriteLine("What would you like to do?");
+Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
 
-Console.WriteLine("Provide a number: ");
+//reads line from console and assigns to variable
 string userInput = Console.ReadLine();
-int num = int.Parse(userInput);
-Console.WriteLine(num);
-
-//Console.WriteLine("Hello!");
-//Console.WriteLine("What would you like to do?");
-//Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-");
-//Console.WriteLine("[S]ee all TODOs");
-//Console.WriteLine("[A]dd a TODO");
-//Console.WriteLine("[R]emove a TODO");
-//Console.WriteLine("[E]xit");
-
-////reads line from console and assigns to variable
-//string userInput = Console.ReadLine();
 
 
-////require keypress to continue program, in this case terminate
-//Console.ReadKey();
+//require keypress to continue program, in this case terminate
+Console.ReadKey();
