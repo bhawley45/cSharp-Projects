e421d59cad64642f72b3e5baf4e9fdd82accbdb5
bhawley45 <136668039+bhawley45@users.noreply.github.com>
2023-10-07 16:55:38 -0500

    Test Web Application using .cshtml: - Basic HTML page w/ embedded music video - Basic customized CSS properties - Simple JavaScript animation

---
+++ Basic_C#_Projects/WebAppTutorial/WebAppTutorial/wwwroot/js/site.js
@@ -0,0 +1,23 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+function newColor() {
+    document.getElementById("hello_world").style.color = `blue`;
+}
+
+function oldColor() {
+    document.getElementById("hello_world").style.color = "darkolivegreen";
+}
+
+/* Logic for movable text */
+const elementToMove = document.querySelector('.movable');
+
+elementToMove.addEventListener('mouseover', () => {
+    elementToMove.classList.add('hovered');
+});
+
+elementToMove.addEventListener('mouseout', () => {
+    elementToMove.classList.remove('hovered');
+});
