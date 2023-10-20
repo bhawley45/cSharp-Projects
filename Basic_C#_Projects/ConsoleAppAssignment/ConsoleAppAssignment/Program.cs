b1bffa94be5a0b5516b907c3529044e070b485fc
bhawley45 <136668039+bhawley45@users.noreply.github.com>
2023-10-20 16:03:00 -0500

    Created ExceptionHandling and ConsoleAppAssignment
    ExceptionHandling: Simple app for learning try/catch blocks (completed)
    
    ConsoleAppAssingment: Throwing a weird error where my antivirus thinks it's a trojan, need to troubleshoot but project seems inconsequential currently

---
+++ Basic_C#_Projects/ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ConsoleAppAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] strings = { "apple", "pear", "orange" };
+
+            //Console.WriteLine("Enter a verb: ");
+            //var verb = Console.ReadLine();
+
+            //for(int i = 0; i < strings.Length; i++)
+            //{
+            //    strings[i] += (" " + verb);
+            //}
+
+            ////Print each element in strings[]
+            //foreach(string s in strings)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+
+            //Console.WriteLine("Please enter the number 2: ");
+            //int input = int.Parse(Console.ReadLine());
+            //while (input < 2)
+            //{
+            //    Console.WriteLine("Dawg that's not 2...");
+            //    input = int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine("Bro, 2 and greater are the best numbers");
+
+            //Console.WriteLine("Please enter a number >5: ");
+            //input = int.Parse(Console.ReadLine());
+            //while (input <= 5)
+            //{
+            //    Console.WriteLine("Dawg that's not >5...");
+            //    input = int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine("Bro, 5 and lesser numbers stink...");
+
+
+
+            List<string> myStrings = new List<string>() { "apples", "bananas", "volkswagen", "ford", "ford" };
+            List<string> duplicates = new List<string>();
+            Console.WriteLine("search list:");
+            var text = Console.ReadLine();
+
+            //make duplicate string string...
+            for(int i = 0; i < myStrings.Count; i++)
+            {
+                for(int j = 0; j < myStrings[i].Length; j++)
+                {
+                    if (myStrings[i] == myStrings[j])
+                    {
+                        //Add duplicate string to appropriate list
+                        duplicates.Add(myStrings[i]);
+                    }
+                }
+            }
+
+            while (!myStrings.Contains(text))
+            {
+                Console.WriteLine($"{text} does not exist in the provided list. \nPlease try again... \n");
+
+                //retry input
+                text = Console.ReadLine();
+            }
+            Console.WriteLine($"\nYour text, {text}, exists at index {myStrings.IndexOf(text)}");
+
+            //Print duplicates
+            Console.WriteLine("\nDuplicates:");
+            foreach(var item in duplicates)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Basic_C#_Projects/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //    Console.WriteLine("Pick a number:");
+            //    int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("Pick a second number:");
+            //    int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //    Console.WriteLine("Dividing the two...");
+
+            //    int num3 = num1 / num2;
+            //    Console.WriteLine($"{num1} / {num2} = {num3}");
+            //}
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    Console.WriteLine("Please type a whole number...");
+            //    return;
+            //}
+            //catch(DivideByZeroException ex)
+            //{
+            //    Console.WriteLine("Please don't divide by 0!");
+            //}
+            ////General exception handling
+            //catch(Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            ////finally will run no matter what, when an error is caught you might want to do a different action
+            //finally
+            //{
+            //    //typically a DB log would be here (ex. credit card transaction)
+            //    Console.ReadKey();
+            //}
+
+
+            /*
+             * Create list of ints
+             * ask for number to divide each element in list by
+             * loop to divide each item in list
+             * display to screen
+             * build try/catch block to print message for each exception
+             * exceptions to check: enter 0, enter a string, maybe others?
+            */
+
+            List<int> ints = new List<int>() {1,4,9,3};
+            try
+            {
+                Console.WriteLine("Please enter a num to divide by:");
+                int divisor = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int i in ints)
+                {
+                    Console.WriteLine(i / divisor);
+                }
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("Please don't divide by 0!");
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}
