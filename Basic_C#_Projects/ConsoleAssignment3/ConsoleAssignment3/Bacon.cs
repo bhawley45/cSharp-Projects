b3065e77d59ba0ce944eab0390991489b7459b83
bhawley45 <136668039+bhawley45@users.noreply.github.com>
2023-10-23 18:32:55 -0500

    Created ConsoleAssignment3
    Really weird multi-part assignment... (completed as per requirements)

---
+++ Basic_C#_Projects/ConsoleAssignment3/ConsoleAssignment3/Bacon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAssignment3
+{
+    public static class Bacon
+    {
+        public static int addNums(int a, int b)
+        {
+            if(b == 0)
+            {
+                return a + 5;
+            }
+            return a + b;
+        }
+
+        public static int displayAndAddTwo(int a, int b)
+        {
+            Console.WriteLine($"second argument {b}");
+            return a + 2;
+        }
+    }
+}
---
+++ Basic_C#_Projects/ConsoleAssignment3/ConsoleAssignment3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAssignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"output: {Bacon.displayAndAddTwo(2,5)}");
+
+            int num1 = getNum();
+            Console.WriteLine("\nType 0 to skip... (adds 5)");
+            int num2 = getNum();
+
+            Console.WriteLine(Bacon.addNums(num1, num2));
+            Console.ReadKey();
+        }
+
+        public static int getNum()
+        {
+            Console.WriteLine("\nPlease enter a number:");
+
+            int num = -1;
+            while (num == -1)
+            {
+                string input = Console.ReadLine();
+                try
+                {
+                    num = Convert.ToInt32(input);
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter a valid integer...");
+                }
+            }
+            return num;
+        }
+    }
+}
