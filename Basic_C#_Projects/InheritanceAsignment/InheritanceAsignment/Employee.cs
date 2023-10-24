79b832fd97fc7acfce041a9e6ea2fe20905d46da
bhawley45 <136668039+bhawley45@users.noreply.github.com>
2023-10-23 19:24:38 -0500

    Created InheritanceAssignment
    Initial commit, finished project as per requirements

---
+++ Basic_C#_Projects/InheritanceAsignment/InheritanceAsignment/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAsignment
+{
+    internal class Employee : Person
+    {
+        public int IdNumber { get; set; }
+    }
+}
