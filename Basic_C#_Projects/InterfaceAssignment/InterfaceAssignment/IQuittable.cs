a869d6b049e171204978b3e77cb660c9b8771501
bhawley45 <136668039+bhawley45@users.noreply.github.com>
2023-10-25 21:59:17 -0500

    Added InterfaceAssignment
    completed...

---
+++ Basic_C#_Projects/InterfaceAssignment/InterfaceAssignment/IQuittable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAssignment
+{
+    internal interface IQuittable
+    {
+        //All methods in Interface are public
+        void Quit();
+    }
+}
