d88e05883182ff5fb5bebae5d6069c33646ba9a4
bhawley45 <136668039+bhawley45@users.noreply.github.com>
2023-11-06 19:18:22 -0600

    Updated InheritanceAssignment.cs according to instructions in desc.
    1. In the Main() method, create a list of at least 10 employees. Each employee should have a first and last name, as well as an Id. At least two employees should have the first name “Joe”.
    
    2. Using a foreach loop, create a new list of all employees with the first name “Joe”.
    
    3. Perform the same action again, but this time with a lambda expression.
    
    4. Using a lambda expression, make a list of all employees with an Id number greater than 5.

--- Basic_C#_Projects/InheritanceAsignment/InheritanceAsignment/Program.cs
+++ Basic_C#_Projects/InheritanceAsignment/InheritanceAsignment/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +17,41 @@ namespace InheritanceAsignment
             employee2.SayName();
 
             Console.WriteLine(employee1 == employee2);
+
+            
+            Employee employee3 = new Employee() {  FirstName = "Joe", LastName = "Bob", IdNumber= 56 };
+            Employee employee4 = new Employee() {  FirstName = "Joe", LastName = "Slap", IdNumber= 57 };
+            Employee employee5 = new Employee() {  FirstName = "Jimmy", LastName = "Bobby", IdNumber= 58 };
+            Employee employee6 = new Employee() {  FirstName = "Bob", LastName = "Top", IdNumber= 59 };
+            Employee employee7 = new Employee() {  FirstName = "Tim", LastName = "When", IdNumber= 60 };
+            Employee employee8 = new Employee() {  FirstName = "Jim", LastName = "Mario", IdNumber= 61 };
+            Employee employee9 = new Employee() {  FirstName = "Pen", LastName = "Luigi", IdNumber= 62 };
+            Employee employee10 = new Employee() {  FirstName = "Quick", LastName = "Bob", IdNumber= 63 };
+
+            List<Employee> employees = new List<Employee>() 
+            {
+                employee1, employee2, employee3,
+                employee4, employee5, employee6,
+                employee7, employee8, employee9,
+                employee10
+            };
+
+            //List<Employee> joeEmployees = new List<Employee>();
+            //foreach (Employee employee in employees)
+            //{
+            //    if (employee.FirstName.Equals("Joe"))
+            //    {
+            //        joeEmployees.Add(employee);
+            //        Console.WriteLine($"{employee.FirstName} {employee.LastName}");
+            //    }
+            //}
+
+            List<Employee> joeEmployee = employees.Where(employee => employee.FirstName.Equals("Joe")).ToList();
+
+            foreach (Employee employee in joeEmployee)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName}");
+            }
 
             Console.ReadKey();
         }
---
+++ Basic_C#_Projects/StructChallenge/StructChallenge/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Number myNum = new Number();
+            myNum.Amount = 1;
+            Console.WriteLine(myNum.Amount);
+
+            Console.ReadLine();
+        }
+
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+    }
+}
