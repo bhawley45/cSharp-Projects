60132b339ed1602bafe6b7211fae88b6e033cbca
bhawley45 <136668039+bhawley45@users.noreply.github.com>
2023-11-09 01:41:53 -0600

    Created InputChallenge and completed according to requirements
    1. Asks the user for a number.
    2. Logs that number to a text file.
    3. Prints the text file back to the user.

---
+++ Basic_C#_Projects/InputChallenge/InputChallenge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InputChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the number input program :)");
+            Console.WriteLine(" - you enter the number, we give it back -");
+            var input = Console.ReadLine();
+
+            File.WriteAllText(@"C:\Users\bhawl\Downloads\Log\log.txt", $"user input: {input}");
+            Console.WriteLine(File.ReadAllText(@"C:\Users\bhawl\Downloads\Log\log.txt"));
+
+            Console.ReadKey();
+        }
+    }
+}
