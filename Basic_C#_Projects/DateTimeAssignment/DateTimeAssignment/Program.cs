6db8baf12f980644156f36480bebba9ade166647
bhawley45 <136668039+bhawley45@users.noreply.github.com>
2023-11-09 02:19:50 -0600

    Created DateTimeAssignment.cs and completed according to requirements
    1. Prints the current date and time to the console.
    2. Asks the user for a number.
    3. Prints to the console the exact time it will be in X hours, X being the number the user entered in step 2.

---
+++ Basic_C#_Projects/DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentTime = DateTime.Now;
+            Console.WriteLine($"The current time is: {currentTime}\n");
+            Console.WriteLine("How many hours would you like to add?");
+            var numOfHours = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"The time in {numOfHours} hours will be {currentTime.AddHours(numOfHours)}");
+            Console.ReadKey();
+        }
+    }
+}
